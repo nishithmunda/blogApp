6557c1c70dd3ccbc6ba46c83cc262ff3f4a5a3e2
nishithmunda <nishithmunda9212@gmail.com>
2021-03-12 10:23:07 +0530

    bug fix

--- src/index.js
+++ src/index.js
@@ -9,15 +9,15 @@ import Amplify from 'aws-amplify';
 
 // Amplify.configure(aws_exports)
 Amplify.configure({
-  'aws_appsync_graphqlEndpoint': 'https://7pmp2ivbbzblvcpmy4vgrnxxpi.appsync-api.us-east-1.amazonaws.com/graphql',
-  'aws_appsync_region': 'us-east-1',
-  'aws_appsync_authenticationType': 'AMAZON_COGNITO_USER_POOLS', // You have configured Auth with Amazon Cognito User Pool ID and Web Client Id
+  'aws_appsync_graphqlEndpoint': '',
+  'aws_appsync_region': '',
+  'aws_appsync_authenticationType': '', // You have configured Auth with Amazon Cognito User Pool ID and Web Client Id
   Auth: {
-      identityPoolId: 'us-east-1:eb1f0a50-ffff-400d-a031-db541ea783ff',
-      region: 'us-east-1',
-      identityPoolRegion: 'us-east-1',
-      userPoolId: 'us-east-1_QV35FoKLd',
-      userPoolWebClientId: '2cc6njvf6o12837l1qpscgdoel',
+      identityPoolId: '',
+      region: '',
+      identityPoolRegion: '',
+      userPoolId: '',
+      userPoolWebClientId: '',
       // cookieStorage: {
       //   domain: 'xxx',
       //   path: 'xxx',
